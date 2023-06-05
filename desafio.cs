d641c91d0e38ea998076a6887ec64ac746716c78
Pulsebl <121182633+Pulsebl@users.noreply.github.com>
2023-06-05 16:05:04 -0300

    Create desafio.cs

---
+++ desafio.cs
@@ -0,0 +1,13 @@
+let est = [10,23,15,78]
+    let vend = [3,20,10,13]
+    let resul = []
+    
+    const rest = (est, vend)=>{
+            for (let i in est){
+                resul.push(est[i]- vend[i])
+            }
+            console.log('Resultados restantes: ')
+            return resul
+
+    }
+    console.log(rest(est,vend))
